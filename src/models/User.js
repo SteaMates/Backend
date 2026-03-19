e7864837335fd17727cb79cb9523ce02894e70ce
Nasarre <869561@unizar.es>
2026-03-19 23:18:21 +0100

    roles

--- src/models/User.js
+++ src/models/User.js
@@ -31,6 +31,11 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
 });
 
 export default mongoose.model('User', userSchema);
